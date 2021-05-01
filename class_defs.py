61bc441061ffa933a1218a7bc80e639116a11c50
CapOfWeird <ninjakirby440@gmail.com>
2021-05-01 19:41:36 -0400

    Added initialization to classes without it

--- class_defs.py
+++ class_defs.py
@@ -2,33 +2,34 @@
 
 # Define equipment class
 class Equipment:
-    name = ''      # Equipment name
-    equip_id = -1  # Frankly I don't even know if I'll need this property
-    value = 0      # Base equipment price
-    position = 0   # Numerical representation of equip position
-    traits = []    # Any additional traits attached to the equipment
-    stats = {
-        'max_hull': 0,
-        'cargo_space': 0,
-        'fuel_rate': 0,
-        'speed': 0,
-    }
+
+    def __init__(self, name: str, equip_id: int, value: int,
+                 position: int, traits: list, stats: dict):
+        self.name = name  # Equipment name
+        self.equip_id = equip_id  # ID used to manage equipment
+        self.value = value  # Base equipment price
+        self.position = position  # Numerical representation of equip position
+        self.traits = traits  # Any additional traits attached to the equipment
+        self.stats = stats  # Stats changed by the equipment and by how much
 
 
 # Define item class
 class Item:
-    name = ''     # Item name
-    item_id = -1  # ID used to manage items
-    value = 0     # Base price for the item
-    traits = []   # List of traits the item has
+
+    def __init__(self, name: str, item_id: int, value: int, traits: list):
+        self.name = name  # Item name
+        self.item_id = item_id  # ID used to manage items
+        self.value = value  # Base price for the item
+        self.traits = traits  # List of traits the item has
 
 
 # Define location class
-class Location(object):
+class Location:
 
-    def __init__(self, name, coords, faction, will_buy, selling):
-        self.name = name        # Location name
-        self.coords = coords    # X and Y coordinates for the location
+    def __init__(self, name: str, coords: list, faction: tuple,
+                 will_buy: list, selling: list):
+        self.name = name  # Location name
+        self.coords = coords  # X and Y coordinates for the location
         self.faction = faction  # Location primary faction
 
         self.will_buy = will_buy  # List of item IDs this location will buy
@@ -37,24 +38,38 @@ class Location(object):
 
 # Define Sailor class
 class Sailor:
-    first_name = ""  # Sailor's first name
-    last_name = ""   # Sailor's last name
-    ship_role = ""   # Their job on the ship
-    faction = ""     # The faction their allegiance is primarily given to
-    is_spy = False   # Whether or not they are a spy
-    hit_point_max = 10
-    hit_points = hit_point_max
-    veils = 10
-    mirrors = 10
 
-    inventory = []
+    def __init__(self, first_name: str, last_name: str, ship_role: str,
+                 faction: tuple, is_spy: bool, hit_point_max: int,
+                 veils: int, mirrors: int):
+        # Sailor's first name
+        self.first_name = first_name
+        # Sailor's last name
+        self.last_name = last_name
+        # Their job on the ship
+        self.ship_role = ship_role
+        # The faction their allegiance is primarily given to
+        self.faction = faction
+        # Whether or not they are a spy
+        self.is_spy = is_spy
+        # Hit points indicate whether or not the sailor is dead
+        self.hit_point_max = hit_point_max
+        self.hit_points = self.hit_point_max
+        # Veils are used for deception and cunning
+        self.veils = veils
+        # Mirrors are used for navigation and perception
+        self.mirrors = mirrors
+
+        self.inventory = []
 
 
 # Define Ship class
-class Ship(object):
+class Ship:
 
-    def __init__(self, name, ship_class, faction, max_hull, cargo_space,
-                 equip_positions, fuel_rate, speed):
+    def __init__(self, name: str, ship_class: str, faction: tuple,
+                 max_hull: int, cargo_space: int, equip_positions: list,
+                 weight: int, fuel_rate, speed):
+        # The name of the ship
         self.name = name
         # The class of the ship, e.g. battleship, merchant cruiser
         self.ship_class = ship_class
@@ -68,6 +83,8 @@ class Ship(object):
         self.cargo_space = cargo_space
         # The available slots that weapons or equipment can be used
         self.equip_positions = equip_positions
+        # A ship's weight affects its speed
+        self.weight = weight
         # A fuel_rate of 2 means this ship travels 2 squares per fuel
         self.fuel_rate = fuel_rate
         # A speed of 1 means this ship travels 1Nm/Hr
